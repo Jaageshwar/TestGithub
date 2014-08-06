34f7c8eea664acc567a34d86f99718bf2e93f2ed
RajkumarPunchh <rajkumar@punchh.com>
2014-08-06 11:06:35 +0530

    Third Data Model added.

---
+++ GitTest/ThirdDataModel.h
@@ -0,0 +1,13 @@
+//
+//  ThirdDataModel.h
+//  GitTest
+//
+//  Created by Punchh on 8/6/14.
+//  Copyright (c) 2014 Punchh12. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThirdDataModel : NSObject
+
+@end
---
+++ GitTest/ThirdDataModel.m
@@ -0,0 +1,13 @@
+//
+//  ThirdDataModel.m
+//  GitTest
+//
+//  Created by Punchh on 8/6/14.
+//  Copyright (c) 2014 Punchh12. All rights reserved.
+//
+
+#import "ThirdDataModel.h"
+
+@implementation ThirdDataModel
+
+@end
