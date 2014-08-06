f783c9e8fc0ef58946f313d2d259fcedcf8719e3
RajkumarPunchh <rajkumar@punchh.com>
2014-08-06 11:06:04 +0530

    Second data model added.

---
+++ GitTest/SecondModel.m
@@ -0,0 +1,13 @@
+//
+//  SecondModel.m
+//  GitTest
+//
+//  Created by Punchh on 8/6/14.
+//  Copyright (c) 2014 Punchh12. All rights reserved.
+//
+
+#import "SecondModel.h"
+
+@implementation SecondModel
+
+@end
