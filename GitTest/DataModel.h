8658e75778ef8a516bbaa800b9f92ba823a1afd2
RajkumarPunchh <rajkumar@punchh.com>
2014-08-06 11:00:21 +0530

    Data model class added.

---
+++ GitTest/DataModel.h
@@ -0,0 +1,13 @@
+//
+//  DataModel.h
+//  GitTest
+//
+//  Created by Punchh on 8/6/14.
+//  Copyright (c) 2014 Punchh12. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataModel : NSObject
+
+@end
